2c5afb9606b8e98d53ed7ab702ce547a3640be48
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-20 17:12:38 +0000

    automatically backed up by learn

--- db/migrate/20180420164524_create_movies.rb
+++ db/migrate/20180420164524_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration
       t.integer :release_date
       t.string :director
       t.string :lead
-      t.string :in_theaters
+      t.boolean :in_theaters
     end
   end
 end
