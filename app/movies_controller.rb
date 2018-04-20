b0b37c4f6897a176d30fe6b82d17cf62bce66def
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-20 17:52:09 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -40,7 +40,7 @@ def can_be_created_in_a_block(args = {})
 end
 
 def can_get_the_first_item_in_the_database
-  return Movie.find_by(id: 1).title
+  return Movie.find_by(id: Movie.first.id).title
 end
 
 def can_get_the_last_item_in_the_database
